add0609608be75b92591b93dc9ba593fbbe59295
Chinmaya <user@Rentomojos-MacBook-Air.local>
2021-07-27 09:36:40 +0530

    added docker compose for redis

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const redis = require("redis");
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({ host: "redis-server", port: 6379 });
 
 //Set initial visits
 client.set("visits", 0);
@@ -14,8 +14,8 @@ app.get("/", (req, res) => {
 
 app.get("/topselling/category/:categoryId", (req, res) => {
   client.get("visits", (err, visits) => {
-    res.send("Number of visits is: " + visits + 1);
-    client.set("visits", parseInt(visits) + 1);
+    res.send("Number of visits is: " + ++visits);
+    client.set("visits", visits);
   });
 });
 
